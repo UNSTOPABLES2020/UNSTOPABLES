f5d8f98f7b06a81041981e8f03c2a0ea2d2eec63
JBSanjay <jbsanjaysanjay579@gmail.com>
2022-05-26 19:59:04 +0530

    sanjay created javascript folder

---
+++ html files/javascript26.05/js/26.05.22trysanjay.js
@@ -0,0 +1,6 @@
+function loginValue() {
+    var username = document.getElementById("name").value;
+    var password = document.getElementById("password").value;
+    console.log([username, password]);
+
+}
