7aa964f609ea73b0f420639ba08cce8cdf2b4df5
JBSanjay <jbsanjaysanjay579@gmail.com>
2022-05-27 19:15:51 +0530

    sanjay added 27.05 js file

--- html files/javascript26.05/js/26.05.22trysanjay.js
+++ html files/javascript26.05/js/26.05.22trysanjay.js
@@ -1,6 +1,7 @@
 function loginValue() {
     var username = document.getElementById("name").value;
     var password = document.getElementById("password").value;
-    console.log([username, password]);
+    var year = document.getElementById("2ndyear").value;
+    console.log([username, password, 2 ndyear]);
 
 }
---
+++ html files/javascript26.05/js/27.05cssanjay.js
@@ -0,0 +1,17 @@
+function loginvalue() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    var genderm = document.getElementById("Male").value;
+    var genderf = document.getElementById("female").value;
+    console.log({ "username": username, "password": password, "male": genderm, "female": genderf })
+}
+var studentid = [{
+    username: "jbsanjaysanjay579@gmail.com",
+    password: "sanjay5793",
+} {
+    username: "sanjay579",
+    password: "hello",
+} {
+    username: "hari123",
+    password: "hi",
+}]
