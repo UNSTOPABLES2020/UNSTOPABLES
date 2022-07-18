186d5a8a8ba366eedebc31d38c82963d2e738bb3
JBSanjay <jbsanjaysanjay579@gmail.com>
2022-07-18 14:02:58 +0530

    task related fetch from api file is added

--- Javascript/Sanjay/sanjay.js
+++ Javascript/Sanjay/sanjay.js
@@ -1,1 +1,16 @@
-hello
+    fetch("https://www.breakingbadapi.com/api/characters?limit=10").then((data) => {
+        return data.json();
+    }).then((wholedata) => {
+        let data1 = "";
+        wholedata.map((value) => {
+            data1 += ` <div class="container>
+            <img src = "${value.img}" class = "profile" alt = "img" >
+                <p class = "name" > ${value.name} </p> 
+                <p class = "birthday" >${value.birthday} </p> 
+                <p class = "occupation" >${value.occupation} </p>
+                </div> `;
+        });
+        document.getElementById("cards").innerHTML = data1;
+    }).catch((err) => {
+        console.log(err);
+    })
